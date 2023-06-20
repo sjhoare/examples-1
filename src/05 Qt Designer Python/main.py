20051e2b2f292cc320c7e2b61bfa3d3e9e2e2b72
Steve Hoare <sj_hoare@hotmail.com>
2023-06-20 21:12:31 +0100

    Changed path to dialog.ui

--- src/05 Qt Designer Python/main.py
+++ src/05 Qt Designer Python/main.py
@@ -1,7 +1,7 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 
-Form, Window = uic.loadUiType("dialog.ui")
+Form, Window = uic.loadUiType(r".\PyQt\examples\src\05 Qt Designer Python\dialog.ui")
 
 app = QApplication([])
 window = Window()
